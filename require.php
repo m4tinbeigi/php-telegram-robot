ffab05e785987ce4c2e8fed63912f0c715d66c82
Rick Sanchez <113212040+m4tinbeigi@users.noreply.github.com>
2023-04-30 22:17:00 +0430

    change base file

---
+++ require.php
@@ -0,0 +1,7 @@
+<?php
+require_once('vendor/autoload.php');
+require_once('app/index.php');
+require_once('config/index.php');
+require_once('controller/index.php');
+require_once('model/index.php');
+require_once('view/index.php');
